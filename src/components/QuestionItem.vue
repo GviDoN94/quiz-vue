2c8093e0f71f22f9fbdcb20615df0d15bedfe65b
Vladislav <vlad_surin@mail.ru>
2023-08-15 17:51:55 +0300

    Display info question

---
+++ src/components/QuestionItem.vue
@@ -0,0 +1,54 @@
+<script setup>
+  const { question } = defineProps(['question']);
+</script>
+
+<template>
+  <div class="question-container">
+    <h1 class="question">{{ question.text }}</h1>
+  </div>
+  <div class="options-container">
+    <div
+      v-for="option in question.options"
+      :key="option.id"
+      class="option"
+    >
+      <p class="option-label">{{ option.label }}</p>
+      <div class="option-value">
+        <p>{{ option.text }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .question-container {
+    margin-top: 20px;
+  }
+
+  .question {
+    font-size: 40px;
+  }
+
+  .option {
+    display: flex;
+    margin-bottom: 20px;
+    cursor: pointer;
+  }
+
+  .option-label {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 50px;
+    font-size: 30px;
+    background-color: bisque;
+  }
+
+  .option-value {
+    padding: 0 20px;
+    width: 100%;
+    font-size: 30px;
+    background-color: rgb(244, 239, 239);
+  }
+</style>
--- src/pages/QuizPage.vue
+++ src/pages/QuizPage.vue
@@ -1,94 +1,22 @@
+<script setup>
+  import QuestionHeader from '@/components/QuestionHeader.vue';
+  import QuestionItem from '@/components/QuestionItem.vue';
+  import { useRoute } from 'vue-router';
+  import quizesData from '@/data/data.json';
+  import { ref } from 'vue';
+
+  const route = useRoute();
+  const quizId = +route.params.id;
+  const questions = quizesData.find((quiz) => quiz.id === quizId).questions;
+  const currentQuestionIndex = ref(0);
+  const questionStatus = `${currentQuestionIndex.value}/${questions.length}`;
+</script>
+
 <template>
   <div>
-    <header>
-      <h4>Question 1/3</h4>
-      <div class="bar">
-        <div class="completion"></div>
-      </div>
-    </header>
+    <QuestionHeader :questionStatus="questionStatus" />
     <div>
-      <div class="question-container">
-        <h1 class="question">What is the chemical value of table salt?</h1>
-      </div>
-      <div class="options-container">
-        <div class="option">
-          <p class="option-label">A</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-        <div class="option">
-          <p class="option-label">B</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-        <div class="option">
-          <p class="option-label">C</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-        <div class="option">
-          <p class="option-label">D</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-      </div>
+      <QuestionItem :question="questions[currentQuestionIndex]" />
     </div>
   </div>
 </template>
-
-<style scoped>
-  header {
-    margin-top: 20px;
-  }
-
-  header h4 {
-    font-size: 30px;
-  }
-
-  .bar {
-    width: 300px;
-    height: 50px;
-    border: 3px solid bisque;
-  }
-
-  .completion {
-    width: 0;
-    height: 100%;
-    background-color: bisque;
-  }
-
-  .question-container {
-    margin-top: 20px;
-  }
-
-  .question {
-    font-size: 40px;
-  }
-
-  .option {
-    display: flex;
-    margin-bottom: 20px;
-    cursor: pointer;
-  }
-
-  .option-label {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 50px;
-    height: 50px;
-    font-size: 30px;
-    background-color: bisque;
-  }
-
-  .option-value {
-    padding: 0 20px;
-    width: 100%;
-    font-size: 30px;
-    background-color: rgb(244, 239, 239);
-  }
-</style>
